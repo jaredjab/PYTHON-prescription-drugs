2ae3c09b8906b9779102b4df3ef100f3d85316c0
jaredjab <jaredbakerbox@gmail.com>
2024-03-21 15:56:59 -0500

    Final Jupyter Notebooks & Maps

--- query_scratchpaper.sql
+++ query_scratchpaper.sql
@@ -24,9 +24,65 @@ SELECT county
     , SUM(total_drug_cost) AS total_opioid_spending
 FROM prescription AS rx
     LEFT JOIN prescriber ON rx.npi = prescriber.npi
+    LEFT JOIN drug ON rx.drug_name = drug.drug_name
+    LEFT JOIN zip_fips AS zf ON prescriber.nppes_provider_zip5 = zf.zip
+    JOIN population AS pop ON zf.fipscounty = pop.fipscounty
+    JOIN fips_county AS fc ON pop.fipscounty = fc.fipscounty
+WHERE opioid_drug_flag = 'Y'
+GROUP BY county;
+
+
+
+
+
+
+
+
+
+
+-- Which county has the most spending overall per capita?
+SELECT county
+    , AVG(population) AS population
+    , SUM(total_drug_cost) AS total_drug_spending
+FROM prescription AS rx
+    FULL JOIN prescriber ON rx.npi = prescriber.npi
+    FULL JOIN zip_fips AS zf ON prescriber.nppes_provider_zip5 = zf.zip
+    FULL JOIN population AS pop ON zf.fipscounty = pop.fipscounty
+    LEFT JOIN fips_county AS fc ON pop.fipscounty = fc.fipscounty
+GROUP BY county;
+
+
+
+WITH a AS (
+SELECT county
+    , AVG(population) AS population
+    , SUM(total_drug_cost) AS total_opioid_spending
+FROM prescription AS rx
+    JOIN prescriber ON rx.npi = prescriber.npi
     JOIN zip_fips AS zf ON prescriber.nppes_provider_zip5 = zf.zip
     JOIN population AS pop ON zf.fipscounty = pop.fipscounty
     JOIN fips_county AS fc ON pop.fipscounty = fc.fipscounty
-    LEFT JOIN drug ON rx.drug_name = drug.drug_name
+    JOIN drug ON rx.drug_name = drug.drug_name
 WHERE opioid_drug_flag = 'Y'
-GROUP BY county;
+GROUP BY county
+)
+
+SELECT SUM(total_opioid_spending)::money AS total_opioid
+FROM a;
+
+SELECT SUM(total_drug_cost)::money AS correct_total
+FROM prescription AS p
+    LEFT JOIN drug AS d ON p.drug_name = d.drug_name
+WHERE opioid_drug_flag = 'Y';
+
+SELECT COUNT(DISTINCT npi) AS Dnpicount,
+    COUNT(npi) AS npicount
+FROM prescriber;
+
+SELECT COUNT(DISTINCT zip) AS Dzip,
+    COUNT(zip) AS zip
+FROM zip_fips;
+
+SELECT * 
+FROM zip_fips
+WHERE zip = '37064'
